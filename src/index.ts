c7ea42e3e2f63f9214511b5c0318bf29ac19c249
BadassBison <shawn.scott.xd@gmail.com>
2020-07-14 17:00:43 -0500

    Ball bounces

--- src/index.ts
+++ src/index.ts
@@ -5,23 +5,55 @@ let ctx = canvasElement.getContext("2d");
 document.body.prepend(canvasElement);
 
 // static varibles
+let speed = 5;
 let x = 50;
+let xSpeed = speed;
 let y = 50;
-ctx.fillStyle = "green";    
+let ySpeed = speed;
+
+let radius = 10;
+
+ctx.fillStyle = "blue";    
 let second = 1000;
-let gameFrames = 60;
-let fps = second / gameFrames;
+let fps = 60;
 
 // draw function
 setInterval(() => {
+    clearCanvas();
+    updateBallPosition();
+    drawBall();
+}, second / fps);
+
+// updates
+
+function clearCanvas() {
     ctx.clearRect(0, 0, innerWidth, innerHeight);
+}
 
-    x += 1;
-    y += 1;
+function updateBallPosition() {
+    x += xSpeed;
+    y += ySpeed;
+    checkBounds();
+}
+
+function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI * 2, true);
+    ctx.arc(x, y, radius, 0, Math.PI * 2, true);
     ctx.fill();
+}
 
-}, fps);
+function checkBounds() {
 
-
+    if (x < 0 + radius) {
+        xSpeed = speed;
+    }
+    if (x > innerWidth - radius) {
+        xSpeed = -speed;
+    }
+    if (y < 0 + radius) {
+        ySpeed = speed;
+    }
+    if (y > innerHeight - radius) {
+        ySpeed = -speed;
+    }
+}
