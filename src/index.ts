d44c4920d8182daaf44f1f6b3bc599dc3b11513a
BadassBison <shawn.scott.xd@gmail.com>
2020-07-14 15:47:56 -0500

    created a ball

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log("this string");
+let canvasElement = document.createElement('canvas');
+canvasElement.width = innerWidth;
+canvasElement.height = innerHeight;
+let ctx = canvasElement.getContext("2d");
+document.body.prepend(canvasElement);
+
+// static varibles
+let x = 50;
+let y = 50;
+ctx.fillStyle = "green";    
+let second = 1000;
+let gameFrames = 60;
+let fps = second / gameFrames;
+
+// draw function
+setInterval(() => {
+    ctx.clearRect(0, 0, innerWidth, innerHeight);
+
+    x += 1;
+    y += 1;
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI * 2, true);
+    ctx.fill();
+
+}, fps);
+
+
