7efea9ea164cbb0e547a53a713f831a481b7d7f2
BadassBison <shawn.scott.xd@gmail.com>
2020-07-16 20:46:11 -0500

    Add circle class

---
+++ src/circle.ts
@@ -0,0 +1,10 @@
+export class Circle {
+    color: string;
+    radius: number;
+
+    constructor(color: string, radius: number) {
+        console.log('new Cirlce');
+        this.color = color;
+        this.radius = radius;
+    }
+}
